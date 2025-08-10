8f56291c6509af58c1e57fb81ef95b0213f93f40
MarcoBroto <msoto9940@gmail.com>
2025-08-10 16:46:58 -0400

    Modified navigation spec file

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -17,10 +17,10 @@ return {
         config = function ()
             local mark = require("harpoon.mark")
             local ui = require("harpoon.ui")
-
+            -- Add menu/add buffer keymaps
             vim.keymap.set("n", "<leader>a", mark.add_file)
             vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
+            -- Add buffer keymaps
             vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
             vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
             vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
@@ -29,9 +29,9 @@ return {
     },
     {
         'mbbill/undotree',
-        config = function ()
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-        end
+        keys = {
+            {'<leader>u', vim.cmd.UndotreeToggle, mode = 'n', desc = "Show undo tree buffer"},
+        },
     },
     {
         "nvim-neo-tree/neo-tree.nvim",
@@ -42,19 +42,85 @@ return {
             "nvim-tree/nvim-web-devicons", -- optional, but recommended
         },
         lazy = false, -- neo-tree will lazily load itself
-        opts = {
-
+        keys = {
+            {'<leader>et', vim.cmd.Neotree, mode = 'n', desc = 'Open neo tree explorer'},
         },
-        config = function()
-            vim.keymap.set('n', '<leader>et', vim.cmd.Neotree)
-            require('neo-tree').setup({
-                filesystem = {
-                    filtered_items = {
-                        hide_dotfiles = false,
-                        hide_hidden = false,
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_hidden = false,
+                },
+            },
+        },
+        --config = function()
+        --    require('neo-tree').setup({
+        --        filesystem = {
+        --            filtered_items = {
+        --                hide_dotfiles = false,
+        --                hide_hidden = false,
+        --            },
+        --        },
+        --    })
+        --end,
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
+        config = function ()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
                     },
                 },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+                messages = {
+                    enabled = false,
+                },
+                popupmenu = {
+                    enabled = false,
+                },
+                notify = {},
+                commands = {},
             })
-        end
-    }
+        end,
+    },
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        -- stylua: ignore
+        keys = {
+            { mode = { "n", "x", "o" }, '<leader>s', function() require("flash").jump() end, desc = "Flash" },
+            { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
 }
