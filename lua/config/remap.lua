4e430565d9b8535cba96137f08dc987bef44cd51
MarcoBroto <msoto9940@gmail.com>
2025-08-10 16:44:59 -0400

    Comment changes

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex) -- Map 'Explore' command
+vim.keymap.set("n", "<leader>ex", vim.cmd.Explore) -- Map 'Explore' command
+vim.keymap.set("n", "<leader>term", vim.cmd.terminal) -- Map 'Terminal' command
