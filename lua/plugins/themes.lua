3dc22d82a9441e13ff2c05297d498b2440ee32b1
MarcoBroto <msoto9940@gmail.com>
2025-08-10 16:43:14 -0400

    Edit themes spec file

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,9 +1,50 @@
+CURRENT_THEME = 'tokyonight'
+
+function setTheme() vim.cmd { cmd = 'colorscheme', args = {CURRENT_THEME} } end
+
 return {
+    {
+        'xiyaowong/transparent.nvim',
+        enabled = false,
+    },
     {
         'rose-pine/neovim',
         name = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    }
+        --config = setTheme,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = setTheme,
+    },
+    {
+        'ribru17/bamboo.nvim',
+        lazy = false,
+        priority = 1000,
+        --config = setTheme,
+    },
+    {
+        "olimorris/onedarkpro.nvim",
+        priority = 1000,
+        --config = setTheme
+    },
+    {'frenzyexists/aquarium-vim'},
+    {'EdenEast/nightfox.nvim'},
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000,
+        --config = setTheme
+    },
+    {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.g.everforest_enable_italic = true
+        --setTheme()
+      end
+    },
 }
