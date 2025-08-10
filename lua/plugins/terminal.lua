5b3708a699fb8a34e203ed78c20aa39fca5d116b
MarcoBroto <msoto9940@gmail.com>
2025-08-10 16:46:01 -0400

    Added terminal plugin spec file

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "google/executor.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {},
+        keys = {
+            {'<leader>er', vim.cmd.ExecutorRunWithNewCommand, mode = 'n', desc = 'Run new Executor command'},
+            {'<leader>ea', vim.cmd.ExecutorRun, mode = 'n', desc = 'Run previou Executor command'},
+            {'<leader>evt', vim.cmd.ExecutorToggleDetail, mode = 'n', desc = 'Toggle Executor Results/Detail View Buffer'},
+        },
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*", 
+        config = true,
+    },
+}
