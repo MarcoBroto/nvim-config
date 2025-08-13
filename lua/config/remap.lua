4fe81de357e077361d2f7141557dc7eccb5d4e1d
MarcoBroto <msoto9940@gmail.com>
2025-08-13 11:54:37 -0400

    Add keymap for relative line number toggling

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,3 +2,4 @@ vim.keymap.set('n', '<leader>ex', vim.cmd.Explore, { desc = 'Open Explorer view'
 vim.keymap.set('n', '<leader>term', vim.cmd.terminal, { desc = 'Open terminal', }) -- Map 'Terminal' command
 vim.keymap.set('n', '<leader>ww', ':set wrap!<CR>', { desc = 'Toggle word wrap', })
 --vim.keymap.set('n', '<M-w>', ':set wrap!<CR>', { desc = 'Toggle word wrap' })
+vim.keymap.set('n', '<leader>ln', ':set relativenumber!<CR>', { desc = 'Toggle ', })
