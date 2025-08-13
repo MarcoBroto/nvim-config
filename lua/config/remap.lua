df68929e76a6f3dc99a8aaa2aedc4fd9c61626e3
MarcoBroto <msoto9940@gmail.com>
2025-08-13 11:41:25 -0400

    Add word wrap keymap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,4 @@
-vim.keymap.set("n", "<leader>ex", vim.cmd.Explore) -- Map 'Explore' command
-vim.keymap.set("n", "<leader>term", vim.cmd.terminal) -- Map 'Terminal' command
+vim.keymap.set('n', '<leader>ex', vim.cmd.Explore, { desc = 'Open Explorer view', }) -- Map 'Explore' command
+vim.keymap.set('n', '<leader>term', vim.cmd.terminal, { desc = 'Open terminal', }) -- Map 'Terminal' command
+vim.keymap.set('n', '<leader>ww', ':set wrap!<CR>', { desc = 'Toggle word wrap', })
+--vim.keymap.set('n', '<M-w>', ':set wrap!<CR>', { desc = 'Toggle word wrap' })
