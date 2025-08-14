1497f8b909f18e126670da3ccfba53d091c6a375
MarcoBroto <msoto9940@gmail.com>
2025-08-14 13:54:34 -0400

    Modified and simplified layout plugin spec

--- lua/plugins/layout.lua
+++ lua/plugins/layout.lua
@@ -14,18 +14,30 @@ local hyperThemeConfig = {
                 key = 'f',
             },
             {
+                --icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Explore',
+                group = 'Label',
+                action = 'Explore',
+                key = 'e',
+            },
+            {
                 desc = 'Keymaps',
                 group = 'Number',
                 action = 'Telescope keymaps',
                 key = 'K',
             },
             {
+                --icon=,
+                --icon_hl=
                 desc = '󰊳 Update',
                 group = '@property',
                 action = 'Lazy update',
                 key = 'u',
             },
             {
+                --icon=,
+                --icon_hl=,
                 desc = 'Health',
                 group = 'Number',
                 action = 'checkhealth',
@@ -97,40 +109,38 @@ return {
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
         },
-        config = function ()
-            require("noice").setup({
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                    },
+        opts = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
                 },
-                -- you can enable a preset for easier configuration
-                presets = {
-                    bottom_search = true, -- use a classic bottom cmdline for search
-                    command_palette = true, -- position the cmdline and popupmenu together
-                    long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false, -- add a border to hover docs and signature help
-                },
-                messages = {
-                    enabled = false,
-                },
-                popupmenu = {
-                    enabled = false,
-                },
-                notify = {},
-                commands = {},
-            })
-        end,
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+            messages = {
+                enabled = false,
+            },
+            popupmenu = {
+                enabled = false,
+            },
+            notify = {},
+            commands = {},
+        }
     },
     {
         'nvimdev/dashboard-nvim',
         event = 'VimEnter',
+        dependencies = { {'nvim-tree/nvim-web-devicons'}},
         opts = DASHBOARD_CONFIG,
-        dependencies = { {'nvim-tree/nvim-web-devicons'}}
     },
     {
         'petertriho/nvim-scrollbar',
@@ -139,5 +149,17 @@ return {
     {
         "folke/zen-mode.nvim",
         enabled = false,
+    },
+    {
+        "folke/twilight.nvim",
+        opts = {},
+        keys = {
+            {'<leader>twi', '<CMD>Twilight<CR>', mode = 'n', desc = 'Toggle Twilight dimming'},
+        },
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {},
     },
 }
