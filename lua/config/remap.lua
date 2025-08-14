200148f71589905042b6307f28b99954de75ea70
MarcoBroto <msoto9940@gmail.com>
2025-08-14 13:48:11 -0400

    Modified remap commands

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set('n', '<leader>ex', vim.cmd.Explore, { desc = 'Open Explorer view', }) -- Map 'Explore' command
 vim.keymap.set('n', '<leader>term', vim.cmd.terminal, { desc = 'Open terminal', }) -- Map 'Terminal' command
-vim.keymap.set('n', '<leader>ww', ':set wrap!<CR>', { desc = 'Toggle word wrap', })
---vim.keymap.set('n', '<M-w>', ':set wrap!<CR>', { desc = 'Toggle word wrap' })
-vim.keymap.set('n', '<leader>ln', ':set relativenumber!<CR>', { desc = 'Toggle ', })
+vim.keymap.set('n', '<leader>ww', '<CMD>set wrap!<CR>', { desc = 'Toggle word wrap', })
+--vim.keymap.set('n', '<M-w>', '<CMD>wrap!<CR>', { desc = 'Toggle word wrap' }) -- Alternative wordwrap keymap using meta key
+vim.keymap.set('n', '<leader>nu', '<CMD>set relativenumber!<CR>', { desc = 'Toggle relative line numbers', })
