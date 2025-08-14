e88ddbb1394efd5ae013803642047f83c9f8e486
MarcoBroto <msoto9940@gmail.com>
2025-08-14 14:10:11 -0400

    Add ToggleTerm command map

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,6 @@
 vim.keymap.set('n', '<leader>ex', vim.cmd.Explore, { desc = 'Open Explorer view', }) -- Map 'Explore' command
-vim.keymap.set('n', '<leader>term', vim.cmd.terminal, { desc = 'Open terminal', }) -- Map 'Terminal' command
+vim.keymap.set('n', '<leader>termv', vim.cmd.terminal, { desc = 'Open terminal buffer', }) -- Map 'Terminal' command
+vim.keymap.set('n', '<leader>term', '<CMD>ToggleTerm<CR>', { desc = 'Toggle terminal half buffer', }) -- Map 'ToggleTerm' command
 vim.keymap.set('n', '<leader>ww', '<CMD>set wrap!<CR>', { desc = 'Toggle word wrap', })
 --vim.keymap.set('n', '<M-w>', '<CMD>wrap!<CR>', { desc = 'Toggle word wrap' }) -- Alternative wordwrap keymap using meta key
 vim.keymap.set('n', '<leader>nu', '<CMD>set relativenumber!<CR>', { desc = 'Toggle relative line numbers', })
