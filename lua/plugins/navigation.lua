0e5fc540a0c0ff6f3fd9eadcdc4fa28afecae263
MarcoBroto <msoto9940@gmail.com>
2025-08-14 13:49:18 -0400

    Cleanup navigation plugin spec

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -53,63 +53,11 @@ return {
                 },
             },
         },
-        --config = function()
-        --    require('neo-tree').setup({
-        --        filesystem = {
-        --            filtered_items = {
-        --                hide_dotfiles = false,
-        --                hide_hidden = false,
-        --            },
-        --        },
-        --    })
-        --end,
-    },
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {},
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        },
-        config = function ()
-            require("noice").setup({
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                    },
-                },
-                -- you can enable a preset for easier configuration
-                presets = {
-                    bottom_search = true, -- use a classic bottom cmdline for search
-                    command_palette = true, -- position the cmdline and popupmenu together
-                    long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false, -- add a border to hover docs and signature help
-                },
-                messages = {
-                    enabled = false,
-                },
-                popupmenu = {
-                    enabled = false,
-                },
-                notify = {},
-                commands = {},
-            })
-        end,
     },
     {
         "folke/flash.nvim",
         event = "VeryLazy",
-        ---@type Flash.Config
-        opts = {},
+        opts = {}, ---@type Flash.Config
         -- stylua: ignore
         keys = {
             { mode = { "n", "x", "o" }, '<leader>s', function() require("flash").jump() end, desc = "Flash" },
